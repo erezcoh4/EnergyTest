9e8dbbf85c9634fcd35650ef1334d2fb4e7bb4de
Erez Cohen <erez@helion.tau.ac.il>
2016-04-09 18:12:49 +0300

    Ssfdsd

--- ETestPackage/runETest.sh
+++ ETestPackage/runETest.sh
@@ -1,10 +1,10 @@
 # operate using
-# > ./runETest <Nbins> <cutoff parameter>
+# > ./runETest <Nbins> <cutoff parameter = 0.6617>
 Nsamples=50
-echo "Nbins = $1, cutoff parameter = $2, Nsamples=$Nsamples"
+echo "Nbins = $1, cutoff parameter = 0.6617, Nsamples=$Nsamples"
 for i in {1..60}
 do
-nohup python mac/calc_ETest.py $1 $Nsamples $i $2 > nohupout/ETest_$1_$2_$i.txt &
+nohup python mac/calc_ETest.py $1 $Nsamples $i > nohupout/ETest_$1_$i.txt &
 echo "launched ETest with Nbins = $1 cutoff parameter $2 number $i"
 done
-echo "done Nbins=$1 cutoff=$2"
+echo "done Nbins=$1 cutoff=0.6617"
