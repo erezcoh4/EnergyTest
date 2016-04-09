c0c06f72a00bc557caece66773d31c4cda47b58d
Erez Cohen <erezcohen@Erezs-iMac.local>
2016-04-09 20:50:51 +0300

    asd

--- ETestPackage/mac/calc_ETest.py
+++ ETestPackage/mac/calc_ETest.py
@@ -18,9 +18,10 @@ rand        = TRandom3(int(sys.argv[3]))
 
 
 DoUniformConst          = False
-DoUniUni                = True
+DoUniUni                = False
 DoUniUniCutOffParameter = False
 DoUniUniContamination   = False
+DoUniUniContCutoffPar   = True
 
 Path    = "/home/erez/EnergyTest/ETestResults"
 #Path    = "/Users/erezcohen/Desktop/EnergyTest/EnergyTestResults"
@@ -32,10 +33,14 @@ elif DoUniUni:
     FileName    = "UniUni_Nbins_%d"%(Nbins)
 elif DoUniUniCutOffParameter:
     CutOffParameter = float(sys.argv[4])
-    FileName    = "UniUni%d_Cutoff%.4f"%(Nbins,CutOffParameter)
+    FileName    = "UniUni%d_Cutoff%.2f"%(Nbins,CutOffParameter)
 elif DoUniUniContamination:
     nContamination = 0.1    # [%] of contammination
     FileName    = "UniGaus%.2fpercentCont_Nbins_%d"%(nContamination,Nbins)
+elif DoUniUniContCutoffPar:
+    CutOffParameter = float(sys.argv[4])
+    nContamination = 0.1    # [%] of contammination
+    FileName    = "Uni_Cutoff%.2f_Gaus%.2fCont_Nbins_%d"%(nContamination,Nbins)
 
 FEtest  = ROOT.TFile(Path+"/"+FileName+"_%d.root"%FileNumber,"recreate");
 TEtest  = ROOT.TTree("ETestTree","ETest statistic "+FileName);
@@ -102,7 +107,7 @@ if DoUniUniCutOffParameter:
 
 
 if DoUniUniContamination:
-
+    
     Npoints = 135000
     hUni = ROOT.TH3F("hUni_%d"%(Nbins),"Uniform Distribution Nbins=%d"%Nbins,Nbins,0,1,Nbins,0,1,Nbins,0,1)
     for j in range(0,Npoints) :
@@ -111,6 +116,26 @@ if DoUniUniContamination:
         hSmpl = ROOT.TH3F("hSmpl_%d_%d"%(sample,Nbins),"Uniform sample Nbins=%d"%Nbins,Nbins,0,1,Nbins,0,1,Nbins,0,1)
         for j in range(0,int(Npoints*(1.-nContamination/100.))) : # uniform
             hSmpl.Fill(rand.Uniform(),rand.Uniform(),rand.Uniform())
+        for j in range(0,int(Npoints*(nContamination/100.))) :    # contamination
+            hSmpl.Fill(rand.Gaus(0.5,0.1),rand.Gaus(0.5,0.1),rand.Gaus(0.5,0.1))
+        Phi[0] = etest.Histo3DETest( hUni , hSmpl )
+        if(sample%1==0):
+            print "Sample %d" %(sample) +" ETest statistic for N=%d bins, sample %d, is %g"%(Nbins,sample,Phi)
+        TEtest.Fill()
+        del hSmpl
+
+
+
+if DoUniUniContCutoffPar:
+    etest.SetCutOffPar(CutOffParameter)
+    Npoints = 1000000
+    hUni = ROOT.TH3F("hUni_%d"%(Nbins),"Uniform Distribution Nbins=%d"%Nbins,Nbins,0,1,Nbins,0,1,Nbins,0,1)
+    for j in range(0,Npoints) :
+        hUni.Fill(rand.Rndm(),rand.Rndm(),rand.Rndm())
+    for sample in range(0,Nsamples) :
+        hSmpl = ROOT.TH3F("hSmpl_%d_%d"%(sample,Nbins),"Uniform sample Nbins=%d"%Nbins,Nbins,0,1,Nbins,0,1,Nbins,0,1)
+        for j in range(0,int(Npoints*(1.-nContamination/100.))) : # uniform
+            hSmpl.Fill(rand.Rndm(),rand.Rndm(),rand.Rndm())
         for j in range(0,int(Npoints*(nContamination/100.))) :    # contamination
             hSmpl.Fill(rand.Gaus(0.5,0.1),rand.Gaus(0.5,0.1),rand.Gaus(0.5,0.1))
         Phi[0] = etest.Histo3DETest( hUni , hSmpl )
